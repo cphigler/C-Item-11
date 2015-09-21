0cc030dfc6de7bc91bc565694ff3f93a1764319a
Cory Phigler <silversurfer1138@yahoo.com>
2015-09-21 12:07:32 -0700

    Uploaded C# item 11

---
+++ Program1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Program1
+{
+    public class Program
+    {
+        // This is where yor program starts.
+        static void Main(string[] args)
+        {
+            // Prompt user to enter a name.
+            Console.WriteLine("Enter your name, please:");
+            // Now read the name entered.
+            string name = Console.ReadLine();
+            // Greet the user with the name that was entered.
+            Console.WriteLine("Hello. " + name);
+            // Wait for the user to acknowledge the results.
+            Console.WriteLine("Press Enter to terminate...");
+            Console.Read();
+        }
+    }
+}
